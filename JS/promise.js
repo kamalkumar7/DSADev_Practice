da3a63646d3eef7c59a1946328b8a69a27cffd44
kamalkumar7 <kk1238405@gmail.com>
2025-04-23 00:00:19 +0530

    promise all

---
+++ JS/promise.js
@@ -0,0 +1,28 @@
+function asyncTask(name, delay, shouldFail = false) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (shouldFail) {
+          reject(`${name} failed`);
+        } else {
+          resolve(`${name} completed`);
+        }
+      }, delay);
+    });
+  }
+  
+  const promise1 = asyncTask("Task 1", 1000);
+  const promise2 = asyncTask("Task 2", 2000);
+  const promise3 = asyncTask("Task 3", 1500);
+  
+
+  // we can use this to run all these 3 asyn tast in parallel
+  Promise.all([promise1, promise2, promise3])
+    .then(results => {
+      console.log("All tasks completed:");
+      console.log(results);
+    })
+    .catch(error => {
+      console.error("At least one task failed:");
+      console.error(error);
+    });
+  
