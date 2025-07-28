1b286efc5ae09a94dc6315a53d86ffb2b455dde8
kamalkumar7 <kk1238405@gmail.com>
2025-07-28 23:58:18 +0530

    memoized funtion

--- JS/memoizeFunction.js
+++ JS/memoizeFunction.js
@@ -16,3 +16,27 @@ var memoizedFunc = memoizedAddTo256();
 
 memoizedFunc(20); // Normal return
 memoizedFunc(20); // Cached return
+
+
+function memoizedMultiply() {
+  var cache = {};
+
+  return function(a, b) {
+    var key = a + ',' + b; // create a unique key for the arguments
+    if (key in cache) {
+      console.log("cached value");
+      return cache[key];
+    } else {
+      var result = a * b;
+      cache[key] = result;
+      return result;
+    }
+  }
+}
+
+var memoizedMul = memoizedMultiply();
+
+console.log(memoizedMul(5, 10)); // Normal return (not cached)
+console.log(memoizedMul(5, 10)); // Cached return
+console.log(memoizedMul(6, 7));  // Normal return (not cached)
+console.log(memoizedMul(6, 7));  // Cached return
